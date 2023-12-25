8493a50a36f60c0dd010e939cd7636f7c0f53e12
bholmesdev <bholmesdev@gmail.com>
2023-12-24 20:07:17 -0500

    feat(style): new sound dropzone

--- src/components/Boards.tsx
+++ src/components/Boards.tsx
@@ -16,13 +16,42 @@ export const editSound = createForm({
   audioFile: audioFileValidator,
 });
 
+function DocumentArrowDown() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-8 h-8"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+      />
+    </svg>
+  );
+}
+
 export function NewSoundDropZone() {
   return (
-    <Form name="new-sound" validator={newSound.validator}>
+    <Form
+      name="new-sound"
+      className="grid place-items-stretch p-3"
+      validator={newSound.validator}
+      style={{ viewTransitionName: "new-sound" }}
+    >
       <FileDropInput
         name="audioFile"
-        className="-z-10 fixed inset-0 border rounded border-gray-200 data-[drop-target]:dark:bg-red-600"
-      ></FileDropInput>
+        className="dark:text-gray-600 py-10 inset-0 data-[drop-target]:dark:bg-gray-900 transition-all data-[drop-target]:scale-105 data-[drop-target]:text-white grid place-items-center rounded-md border dark:border-gray-800 border-dashed"
+      >
+        <span className="flex flex-col items-center gap-3">
+          <DocumentArrowDown />
+          Drag your sounds here
+        </span>
+      </FileDropInput>
     </Form>
   );
 }
