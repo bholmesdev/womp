eb6336788f884a8a47cda89fee392190a9baa39c
bholmesdev <bholmesdev@gmail.com>
2024-04-10 14:18:32 -0400

    chore: completely remove r2

--- src/pages/audio/[key].ts
+++ src/pages/audio/[key].ts
@@ -3,18 +3,18 @@ import type { APIRoute } from "astro";
 export const prerender = false;
 
 export const GET: APIRoute = async (ctx) => {
-  const { key } = ctx.params;
-  if (!key) return new Response(null, { status: 400 });
+  // const { key } = ctx.params;
+  // if (!key) return new Response(null, { status: 400 });
 
-  const { R2 } = ctx.locals.runtime.env;
-  const obj = await R2.get(key);
+  // const { R2 } = ctx.locals.runtime.env;
+  // const obj = await R2.get(key);
 
-  if (obj === null) {
-    return new Response(`${key} not found`, { status: 404 });
-  }
+  // if (obj === null) {
+  //   return new Response(`${key} not found`, { status: 404 });
+  // }
 
-  const headers = new Headers(Object.entries(obj.httpMetadata ?? {}));
-  headers.set("etag", obj.httpEtag);
+  // const headers = new Headers(Object.entries(obj.httpMetadata ?? {}));
+  // headers.set("etag", obj.httpEtag);
 
   return new Response(null);
 };
