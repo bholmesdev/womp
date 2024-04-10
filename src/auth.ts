38cc7e077f1d5a2466932dec411aaf1b3a934a91
bholmesdev <bholmesdev@gmail.com>
2024-04-10 12:52:20 -0400

    wip: secure true for debugging

--- src/auth.ts
+++ src/auth.ts
@@ -15,7 +15,7 @@ export const lucia = new Lucia(adapter, {
   sessionCookie: {
     attributes: {
       // set to `true` when using HTTPS
-      secure: import.meta.env.PROD,
+      secure: true,
     },
   },
   getUserAttributes: (attributes) => {
--- src/pages/login/github/index.ts
+++ src/pages/login/github/index.ts
@@ -14,7 +14,7 @@ export async function GET(context: APIContext): Promise<Response> {
 
   context.cookies.set("github_oauth_state", state, {
     path: "/",
-    secure: import.meta.env.PROD,
+    secure: true,
     httpOnly: true,
     maxAge: 60 * 10,
     sameSite: "lax",
