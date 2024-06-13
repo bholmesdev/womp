2871d1589e57bcd2348eb7ba893786d2594d16e2
bholmesdev <hey@bholmes.dev>
2024-06-13 12:07:48 +0300

    chore: types

--- src/actions.ts
+++ src/actions.ts
@@ -1,12 +1,19 @@
 import { defineAction, z } from "astro:actions";
 import { CO2, db, eq } from "astro:db";
 
+type Result = Array<{
+  client: number;
+  server: number;
+  route: string;
+  referer: string;
+}>;
+
 export const server = {
   getCo2: defineAction({
     input: z.object({
       referer: z.string(),
     }),
-    handler: async ({ referer }) => {
+    handler: async ({ referer }): Promise<Result> => {
       const metrics = await db
         .select({
           client: CO2.client,
